9fca626c7ec8387f38036390a71f478e78867554
Natalia Czapska <czapskanatalia@gmail.com>
2021-12-02 21:38:31 +0100

    Prepare error 404 template.

--- project/urls.py
+++ project/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from register.views import Error404
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("error-404/", Error404.as_view(), name="error-404"),
     path("", include("register.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- register/views.py
+++ register/views.py
@@ -2,4 +2,7 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = "register/home.html"
+    template_name = "register/index.html"
+
+class Error404(TemplateView):
+    template_name = "404.html"
