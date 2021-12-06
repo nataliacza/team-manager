47f3df6a27125feb0b6163be04cb81e6a3c59c4c
Natalia Czapska <czapskanatalia@gmail.com>
2021-12-06 21:19:22 +0100

    Create views for models with templates. Registered urls. Update dynamic links to navbar

--- register/views.py
+++ register/views.py
@@ -1,8 +1,31 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+from register.models import Dog, Equipment, Member
 
 
 class HomeView(TemplateView):
     template_name = "register/index.html"
 
+
 class Error404(TemplateView):
     template_name = "404.html"
+
+
+class TeamListView(ListView):
+    paginate_by = 5
+    model = Member
+    template_name = "register/team.html"
+    context_object_name = "member_list"
+
+
+class DogListView(ListView):
+    paginate_by = 5
+    model = Dog
+    template_name = "register/dogs.html"
+    context_object_name = "dogs_list"
+
+
+class EquipmentListView(ListView):
+    paginate_by = 5
+    model = Equipment
+    template_name = "register/equipment.html"
+    context_object_name = "equipment_list"
