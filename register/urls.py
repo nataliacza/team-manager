17fad5472b11d82886677fc173ee7815e3c8326a
Natalia Czapska <czapskanatalia@gmail.com>
2022-01-11 00:05:27 +0100

    Add new model Fleet, separate from Equipment. Add views, templates and styling.

--- register/urls.py
+++ register/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from register.views import HomeView, DogListView, TeamListView, EquipmentListView, MemberCreateView
+from register.views import HomeView, TeamListView, DogListView, FleetListView, EquipmentListView, MemberCreateView
 
 app_name = "register"
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path("index/", HomeView.as_view(), name="index"),
     path("zespol/", TeamListView.as_view(), name="zespol"),
     path("psy/", DogListView.as_view(), name="psy"),
+    path("flota/", FleetListView.as_view(), name="flota"),
     path("sprzet/", EquipmentListView.as_view(), name="sprzet"),
     path("zespol/dodaj-czlonka/", MemberCreateView.as_view(), name="dodaj-czlonka"),
 ]
